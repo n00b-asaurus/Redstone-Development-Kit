c6682b0a679502d1a6d7cf5faa51d35a3481c87b
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-06-27 12:31:39 -0500

    Removed bulk_import
    This is being handled in its own package

--- Redstone Structure Generator Tool/tests/structure_generator/tools/resources/base_class.py
+++
@@ -1,3 +0,0 @@
-class BaseClass:
-	def __init__(self):
-		pass
--- Redstone Structure Generator Tool/tests/structure_generator/tools/resources/sub_classes/class_c.py
+++
@@ -1,4 +0,0 @@
-from tests.structure_generator.tools.resources.base_class import BaseClass
-class ClassC(BaseClass):
-	def __init__(self):
-		pass
