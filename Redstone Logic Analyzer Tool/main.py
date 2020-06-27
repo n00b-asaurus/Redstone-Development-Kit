c8013bd6370b5491f66fe2026a769fec71878b00
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-06-27 18:29:43 -0500

    Added support for channel name arguments
    This allows the user to specify what channels get rendered in what order in the command line.

--- Redstone Logic Analyzer Tool/main.py
+++ Redstone Logic Analyzer Tool/main.py
@@ -19,9 +19,9 @@ text_loader = TextLoader(log_address)
 timing_collector = TimingCollector(text_loader)
 log.debug("Generating signals")
 signals = timing_collector.get_raw_info()
-visible_signals = [
-	
-]
+visible_signals = []
+if len(visible_signals) > 2:
+	visible_signals = argv[2:]
 if len(visible_signals) > 0:
 	for signal in signals:
 		if signal.name in visible_signals:
