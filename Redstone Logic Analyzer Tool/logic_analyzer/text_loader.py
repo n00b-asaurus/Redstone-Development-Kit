4d05443715ee2dbedf47dcdf4a438e2b5ae06014
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-07-26 17:13:58 -0500

    Removed the logging code
    At this point it's just excessive clutter, I will re-add the logger when I start cleaning up the code

--- Redstone Logic Analyzer Tool/logic_analyzer/text_loader.py
+++ Redstone Logic Analyzer Tool/logic_analyzer/text_loader.py
@@ -1,15 +1,23 @@
+import logging
+
+logger = logging.getLogger()
+
 class TextLoader:
 	def __init__(self, log_address):
+		logger.debug("Constructing TextLoader with log address {}".format(log_address))
 		with open(log_address) as f:
+			logger.debug("Opened file - read lines")
 			log = f.readlines()
+			logger.debug("init text array")
 			self.text = []
 			i = -1
+			logger.debug("Searching for latest start")
 			while "Executed 2 commands from function 'logic_analyzer:commands/start'" not in log[i]: 
-				#print("skipping {}".format(log[i]))
+				logger.debug("skipping {}".format(log[i]))
 				i -= 1
 			i += 1
 			while "Executed 1 commands from function 'logic_analyzer:commands/stop'" not in log[i]: 
-				#print("adding {}".format(log[i]))
+				logger.debug("adding {}".format(log[i]))
 				self.text.append(log[i])
 				i += 1
 		
