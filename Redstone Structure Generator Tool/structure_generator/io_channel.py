f7325da8f6873040422b9f46c3159e330c8090ed
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-08-02 13:16:39 -0500

    Updated IOChannel singleton code
    my toolbox's singleton decorator was not being included in the repository, added it to the code temporarily until I can think of a better place to put it, or a better way of making this class a singleton.

--- Redstone Structure Generator Tool/structure_generator/io_channel.py
+++ Redstone Structure Generator Tool/structure_generator/io_channel.py
@@ -1,5 +1,11 @@
-from toolbox.singleton.singleton import singleton
-
+def singleton(cls):
+	instances = {}
+	def wrapper(*args, **kwargs):
+		if cls not in instances:
+			instances[cls] = cls(*args, **kwargs)
+		return instances[cls]
+	return wrapper
+	
 @singleton
 class IOChannel:
 	def __init__(self,):
