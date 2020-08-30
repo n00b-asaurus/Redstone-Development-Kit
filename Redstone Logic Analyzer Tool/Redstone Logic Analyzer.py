de4242f42cb103474d220d1f47fe44299027ea50
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-08-30 14:20:33 -0500

    Added Failed Channel Reporting to Redstone Logic Analyzer
    This will allow the user to quickly see when a requested channel was not included in the render.

--- Redstone Logic Analyzer Tool/Redstone Logic Analyzer.py
+++ Redstone Logic Analyzer Tool/Redstone Logic Analyzer.py
@@ -3,9 +3,16 @@ from logic_analyzer.screen_renderer import ScreenRenderer
 try:		
 	log = input("Where's latest.log? > ")
 	channels_to_render = input("What channels are being rendered? > ")
-	render = ScreenRenderer().render_log(log, channels_to_render)
+	renderer = ScreenRenderer()
+	render = renderer.render_log(log, channels_to_render)
 	render.save('.\image.png')
 	print("Generation Complete!")
+	failures = renderer.report_failed_signals()
+	print("{} Channels Failed to Render".format(len(failures)))
+	if len(failures) > 0:
+		print("Failed Channels:")
+		for failure in failures:
+			print(failure)
 except Exception as e:
 	print("Encountered Exception: {}".format(e))
 input("Press Enter to Continue...")
