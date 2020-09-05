09701d2464e742ffbb14b07a7bb3eef13f15b00b
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-09-05 18:55:41 -0500

    Cleaned and Refactored Main

--- Redstone Logic Analyzer Tool/Redstone Logic Analyzer.py
+++ Redstone Logic Analyzer Tool/Redstone Logic Analyzer.py
@@ -1,18 +1,38 @@
 from logic_analyzer.screen_renderer import ScreenRenderer
 	
-try:		
-	log = input("Where's latest.log? > ")
-	channels_to_render = input("What channels are being rendered? > ")
-	renderer = ScreenRenderer()
-	render = renderer.render_log(log, channels_to_render)
-	render.save('.\image.png')
-	print("Generation Complete!")
-	failures = renderer.report_failed_signals()
-	print("{} Channels Failed to Render".format(len(failures)))
-	if len(failures) > 0:
+class RedstoneLogicAnalyzer:
+	def __init__(self):
+		self.log_address 		= ""
+		self.channels_to_render = None
+		self.renderer			= ScreenRenderer()
+	
+	def main(self):
+		self._collect_arguments()
+		self._render_and_save_image()
+		self._report_failed_channels()
+	
+	def _collect_arguments(self):
+		self.log_address 		= input("Where's latest.log? > ")
+		self.channels_to_render = input("What channels are being rendered? > ")
+		
+	def _render_and_save_image(self):
+		render = self.renderer.render_log(self.log_address , self.channels_to_render)
+		render.save('.\image.png')
+		print("Generation Complete!")
+		
+	def _report_failed_channels(self):
+		failures = self.renderer.report_failed_signals()
+		print("{} Channels Failed to Render".format(len(failures)))
+		if len(failures) > 0:
+			self._list_failed_channel_names()
+		
+	def _list_failed_channel_names(self):
 		print("Failed Channels:")
-		for failure in failures:
+		for failure in self.renderer.report_failed_signals():
 			print(failure)
+	
+try:		
+	RedstoneLogicAnalyzer().main()
 except Exception as e:
 	print("Encountered Exception: {}".format(e))
 input("Press Enter to Continue...")
