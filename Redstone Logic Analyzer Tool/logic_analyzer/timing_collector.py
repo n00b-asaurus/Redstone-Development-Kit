9ef0adbf8ed84f987acfbf773835bd7129c324b6
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-09-05 18:11:36 -0500

    Cleaned and Refectored TimingCollector

--- Redstone Logic Analyzer Tool/logic_analyzer/timing_collector.py
+++ Redstone Logic Analyzer Tool/logic_analyzer/timing_collector.py
@@ -1,14 +1,4 @@
-class TimingCollector:
-	def __init__(self, text_source):
-		self.text_source 		= text_source
-		self.channels 			= {} #channel template name:{type:comment,signal:{}}
-		self.last_time 			= 0
-		self.time_pattern 		= compile(r'time:(\d*),')
-		self.channel_pattern 	= compile(r'channel:(.*),')
-		self.state_pattern 		= compile(r'state:(.*)$')
-		self.longest_tvalue 	= 0
-		self.signal_classes		= [BitSignal, WordSignal]
-		'''
+'''
 		examples of log output
 		player 				[CHAT] <n00b_asaurus> comment: hello world
 		say executed 		[test_probe] hello world
@@ -21,39 +11,68 @@ class TimingCollector:
 			bit channel:	time:300,channel:probe 1,state:true
 			word channel:	time:300,channel:probe 2,state:23
 			comments: 		time:301,channel:comment,state:write something here!
-		'''
+'''
 
-	
-	def get_raw_info(self):
-		text = self.text_source.get_text() #return as a list, one line per index
-		for line in text:
-			self._parse_line(line)
-		self.longest_tvalue += 20
+class LineDictFactory:
+	def __init__(self):
+		self.channel_dictionary = {}
+		self.longest_tvalue 	= 0
+		self.time_pattern 		= compile(r'time:(\d*),')
+		self.channel_pattern 	= compile(r'channel:(.*),')
+		self.state_pattern 		= compile(r'state:(.*)$')
+		self.signal_classes		= [BitSignal, WordSignal]
 		
-		channel_objects = []
-		for name, data in self.channels.items():
-			channel_objects.append(data["type"](name,data["signal"],self.longest_tvalue))
-		return channel_objects
-	
-	def _parse_line(self, line):
+	def parse_lines(self, lines):
+		for line in lines:
+			self._extract_data_and_add_to_channel_dictionary(line)
+		self.longest_tvalue += 20
+		return self.channel_dictionary, self.longest_tvalue
+		
+	def _extract_data_and_add_to_channel_dictionary(self, line):
 		if "[CHAT]" not in line and "[@]" not in line:
 			return
+		time,channel,state = self._extract_data_from_line(line)
+		self._add_data_to_channel_dictionary(time, channel, state)
+		
+	def _extract_data_from_line(self, line):
 		try:
 			time = int(search(self.time_pattern,line).group(1))
 			channel = search(self.channel_pattern, line).group(1)
 			state = search(self.state_pattern, line).group(1)
 			self.longest_tvalue = max(time,self.longest_tvalue)
+			return time, channel, state
 		except Exception:
 			return
+	
+	def _add_data_to_channel_dictionary(self, time, channel, state):
+		if channel not in self.channel_dictionary:
+			self._create_new_channel(channel, state)
+		self.channel_dictionary[channel]["signal"][time] = self.channel_dictionary[channel]["type"].format_input(state)
 		
-		if channel not in self.channels:
-			self.channels[channel] = {"type":self._match_signal_class(state),"signal":{}}
-		self.channels[channel]["signal"][time] = self.channels[channel]["type"].format_input(state)
-			
+	def _create_new_channel(self, channel, state):
+		self.channel_dictionary[channel] = {"type":self._match_signal_class(state),"signal":{}}
+		
 	def _match_signal_class(self, value):
 		for signal_class in self.signal_classes:
-				if signal_class.match_input(value):
-					return signal_class
+			if signal_class.match_input(value):
+				return signal_class 
+
+
+class TimingCollector:
+	def __init__(self, text_source):
+		self.text_source = text_source
+		
+	def get_raw_info(self):
+		text = self.text_source.get_text() #return as a list, one line per index
+		channel_dictionary,longest_tvalue = LineDictFactory().parse_lines(text)
+		return self._contruct_objects_from_dictionary(channel_dictionary,longest_tvalue)
+		
+	def _contruct_objects_from_dictionary(self,channel_dictionary,longest_tvalue):
+		channel_objects = []
+		for name, data in channel_dictionary.items():
+			channel_objects.append(data["type"](name,data["signal"],longest_tvalue))
+		return channel_objects
+	
 				
 				
 from re import compile, match, search
