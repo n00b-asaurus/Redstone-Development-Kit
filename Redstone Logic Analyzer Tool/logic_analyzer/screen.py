84723655d07d54174aa261a10f43a856dea8aaa3
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-09-05 18:45:58 -0500

    Cleaned and Refactored Screen

--- Redstone Logic Analyzer Tool/logic_analyzer/screen.py
+++ Redstone Logic Analyzer Tool/logic_analyzer/screen.py
@@ -13,26 +13,37 @@ class Screen:
 			self.draw = ImageDraw.Draw(self.screen)
 	
 	def render(self, signals):
-		if type(signals) != list:
-			signals = [signals]
 		if self.size == None:
-			size = (int(signals[0].length * self.horizontal_scale), int((len(signals) * self.vertical_scale * 1.3) + (self.vertical_scale * 0.3))) 
-			self.screen = Image.new(mode = "RGBA", size = size, color = BACKGROUND)
-			self.draw = ImageDraw.Draw(self.screen)
-		for x in range(signals[0].length):
-			self.draw_line([(x,0.0),(x,100.0)],fill = GRID)
+			self._infer_screen_size_from_channels(signals)
+		self._draw_tick_lines(signals[0].length)
+		self._draw_channels(signals)
+		return self.screen
+	
+	def _draw_channels(self,signals):
 		self.channel_offset = 0.3 * self.vertical_scale
 		for signal in signals:
 			signal.render_channel(self)
-			for x in range(0, signal.length,100):
-				self.draw_text(signal.name, (x + 5.0, 0.0))
+			self._draw_channel_name_every_100_pixels(signal)
 			self.channel_offset += 1.3 * self.vertical_scale
-		#still need to draw the timestamps, grids, and the channel headers
-		return self.screen
+			
+	def _draw_channel_name_every_100_pixels(self,signal):
+		for x in range(0, signal.length,100):
+			self.draw_text(signal.name, (x + 5.0, 0.0))
+		
+	def _draw_tick_lines(self, number_of_ticks):
+		for x in range(number_of_ticks):
+			self.draw_line([(x,0.0),(x,100.0)],fill = GRID)
+		
+	def _infer_screen_size_from_channels(self, signals):
+		size = (int(signals[0].length * self.horizontal_scale), int((len(signals) * self.vertical_scale * 1.3) + (self.vertical_scale * 0.3))) 
+		self.screen = Image.new(mode = "RGBA", size = size, color = BACKGROUND)
+		self.draw = ImageDraw.Draw(self.screen)
 		
 	def draw_line(self, coords, fill = SIGNAL):
-		x1, y1 = coords[0][0] * self.horizontal_scale + self.horizontal_offset, coords[0][1] * self.vertical_scale + self.vertical_offset + self.channel_offset
-		x2, y2 = coords[1][0] * self.horizontal_scale + self.horizontal_offset, coords[1][1] * self.vertical_scale + self.vertical_offset + self.channel_offset
+		x1 = coords[0][0] * self.horizontal_scale + self.horizontal_offset
+		y1 = coords[0][1] * self.vertical_scale + self.vertical_offset + self.channel_offset
+		x2 = coords[1][0] * self.horizontal_scale + self.horizontal_offset
+		y2 = coords[1][1] * self.vertical_scale + self.vertical_offset + self.channel_offset
 		self.draw.line([(x1,y1),(x2,y2)], fill = fill, width = 1)
 		
 	def draw_text(self, text, coords):
