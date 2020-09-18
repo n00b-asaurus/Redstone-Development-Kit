81b17db9b1127332e165a258222c23ba9d353b87
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-09-17 21:37:54 -0500

    Updated Structure Generator Tests
    Added tests for various blocks, translation, rotation, and creating multiple commands.

--- Redstone Structure Generator Tool/tests/test_generator.py
+++ Redstone Structure Generator Tool/tests/test_generator.py
@@ -318,4 +318,266 @@ class TestGenerator(TestCase):
 		commands = generate(MockStructure(),argument_handler)
 		self.assertEqual(commands, ["setblock ~77 ~17 ~-23 minecraft:iron_block"])
 		
+	def test_setblock_with_redstone_dust(self):
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [23,17,77]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),redstone_dust()))
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~23 ~17 ~77 minecraft:redstone_wire"])
+		
+	def test_setblock_with_redstone_torch(self):
+		'''assuming the player is facing east (+x), placing a torch on the wall in front of them will cause it to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [23,17,77]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),redstone_torch('-x')))
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~23 ~17 ~77 minecraft:redstone_wall_torch[facing=west]"])
+		
+	def test_setblock_with_redstone_torch_and_rotate_90(self):
+		'''assuming the player is facing east (+x), placing a torch on the wall in front of them will cause it to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [0,0,0,1]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z, rotation):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),redstone_torch('-x')))
+				self._rotate(rotation)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~0 ~0 ~0 minecraft:redstone_wall_torch[facing=north]"])
+		
+	def test_setblock_with_redstone_torch_and_rotate_180(self):
+		'''assuming the player is facing east (+x), placing a torch on the wall in front of them will cause it to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [0,0,0,2]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z, rotation):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),redstone_torch('-x')))
+				self._rotate(rotation)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~0 ~0 ~0 minecraft:redstone_wall_torch[facing=east]"])
+		
+	def test_setblock_with_redstone_torch_and_rotate_270(self):
+		'''assuming the player is facing east (+x), placing a torch on the wall in front of them will cause it to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [0,0,0,3]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z, rotation):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),redstone_torch('-x')))
+				self._rotate(rotation)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~0 ~0 ~0 minecraft:redstone_wall_torch[facing=south]"])
+		
+	def test_setblock_with_repeater(self):
+		'''assuming the player is facing east (+x), placing a repeater on the ground in front of them will cause its output to point east (+x) and its input to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [23,17,77]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),repeater('+x')))
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~23 ~17 ~77 minecraft:repeater[facing=west]"])
+		
+	def test_setblock_with_repeater_and_rotate_90(self):
+		'''assuming the player is facing east (+x), placing a repeater on the ground in front of them will cause its output to point east (+x) and its input to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [0,0,0,1]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z, rotation):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),repeater('+x')))
+				self._rotate(rotation)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~0 ~0 ~0 minecraft:repeater[facing=north]"])
+		
+	def test_setblock_with_repeater_and_rotate_180(self):
+		'''assuming the player is facing east (+x), placing a repeater on the ground in front of them will cause its output to point east (+x) and its input to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [0,0,0,2]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z, rotation):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),repeater('+x')))
+				self._rotate(rotation)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~0 ~0 ~0 minecraft:repeater[facing=east]"])
+		
+	def test_setblock_with_repeater_and_rotate_270(self):
+		'''assuming the player is facing east (+x), placing a repeater on the ground in front of them will cause its output to point east (+x) and its input to point west (-x)'''
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [0,0,0,3]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z, rotation):
+				self._add_command(setblock(Position(pos_x,pos_y,pos_z),repeater('+x')))
+				self._rotate(rotation)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~0 ~0 ~0 minecraft:repeater[facing=south]"])
+		
+	def test_clone_and_translate(self):
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [10,18,15]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z):
+				self._add_command(clone(Position(0,0,0),Position(1,1,1),Position(2,0,2)))
+				self._translate(pos_x,pos_y,pos_z)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["clone ~10 ~18 ~15 ~11 ~19 ~16 ~12 ~18 ~17"])
+	
+	def test_fill_withbase_block_and_translate(self):
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [10,18,15]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z):
+				self._add_command(fill(Position(0,0,0),Position(1,1,1),base_block()))
+				self._translate(pos_x,pos_y,pos_z)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["fill ~10 ~18 ~15 ~11 ~19 ~16 minecraft:iron_block"])
+		
+	def test_setblock_with_base_block_and_translate(self):
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = [10,18,15]
+		
+		class MockStructure(Structure):
+			def generate_build(self, pos_x, pos_y, pos_z):
+				self._add_command(setblock(Position(0,0,0),base_block()))
+				self._translate(pos_x,pos_y,pos_z)
+			
+			def register_arguments(self):
+				return [
+					Argument("PosX", "The X position of the block"),
+					Argument("PosY", "The Y position of the block"),
+					Argument("PosZ", "The Z position of the block"),
+					Argument("Rotation", ""),
+				]
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(commands, ["setblock ~10 ~18 ~15 minecraft:iron_block"])
+		
+	def test_multi_command_structure(self):
+		argument_handler = ArgumentHandler()
+		argument_handler.handle = Mock()
+		argument_handler.handle.return_value = []
+		
+		class MockStructure(Structure):
+			def generate_build(self,):
+				self._add_command(clone(Position(0,0,0),Position(1,1,1),Position(2,0,2)))
+				self._add_command(clone(Position(0,0,0),Position(1,1,1),Position(2,0,2)))
+				self._add_command(clone(Position(0,0,0),Position(1,1,1),Position(2,0,2)))
+				self._add_command(clone(Position(0,0,0),Position(1,1,1),Position(2,0,2)))
+				self._add_command(clone(Position(0,0,0),Position(1,1,1),Position(2,0,2)))
+				self._add_command(clone(Position(0,0,0),Position(1,1,1),Position(2,0,2)))
+			
+			def register_arguments(self):
+				return []
+			
+		commands = generate(MockStructure(),argument_handler)
+		self.assertEqual(len(commands), 6)
+		
 	
