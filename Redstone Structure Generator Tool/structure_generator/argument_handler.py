22f6da7eaf10da2d3a8660853f2ce3f5f501c7ca
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-09-24 18:25:27 -0500

    Refactored argument _handler.py

--- Redstone Structure Generator Tool/structure_generator/argument_handler.py
+++ Redstone Structure Generator Tool/structure_generator/argument_handler.py
@@ -1,22 +1,32 @@
 class ArgumentHandler:
 	def __init__(self):
-		pass
+		self.return_list=True
 		
 	def handle(self, arguments):
-		return_list = True
-		if type(arguments) != list:
-			return_list = False
-			arguments = [arguments]
+		arguments = self._scrub_arguments(arguments)
+		argument_values = self._get_values_from_arguments(arguments)
+		if self.return_list: return argument_values
+		else: return argument_values[0]
+	
+	def _get_values_from_arguments(self, arguments):
 		argument_values = []
 		for argument in arguments:
-			header_format = "{} - {} > ".format(argument.name, argument.help)
-			error_format = "{{}} is not a valid option. Try {}".format(argument.choices)
-			value = self._get_input_and_check_against_valid_options(header_format, argument.choices, error_format)
+			value = self._get_value_from_argument(argument)
 			argument_values.append(value)
-		if return_list:
-			return argument_values
-		else:
-			return argument_values[0]
+		return argument_values
+		
+	def _get_value_from_argument(self, argument):
+		header_format = "{} - {} > ".format(argument.name, argument.help)
+		error_format = "{{}} is not a valid option. Try {}".format(argument.choices)
+		value = self._get_input_and_check_against_valid_options(header_format, argument.choices, error_format)
+		return value
+		
+	def _scrub_arguments(self, arguments):
+		self.return_list = True
+		if type(arguments) != list:
+			self.return_list = False
+			arguments = [arguments]
+		return arguments
 			
 	def _get_input_and_check_against_valid_options(self, header, valid_inputs, error_message_format):
 		while True:
