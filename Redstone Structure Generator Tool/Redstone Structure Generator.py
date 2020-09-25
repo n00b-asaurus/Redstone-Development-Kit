d1782167dbefd67d12d3e59194ba1361ff44862b
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2020-09-24 21:11:25 -0500

    Update Redstone Structure Generator.py

--- Redstone Structure Generator Tool/Redstone Structure Generator.py
+++ Redstone Structure Generator Tool/Redstone Structure Generator.py
@@ -5,18 +5,19 @@ from structures.basic_encoder.encoder import Encoder as BasicEncoder
 from structures.properinglish19_decoder.decoder import Decoder as ProperinglishDecoder
 from structures.stenodyon_decoder.decoder import Decoder as StenodyonDecoder
 from string_handlers.autotyper import AutoTyper
+import traceback
 
 structures = {
 	"basic_encoder": BasicEncoder,
-	"stenodyon_decoder": ProperinglishDecoder,
-	"properinglish19_decoder": StenodyonDecoder,
+	"stenodyon_decoder": StenodyonDecoder,
+	"properinglish19_decoder": ProperinglishDecoder,
 }
 
 argument_handler = ArgumentHandler()
-structure = structures[argument_handler.handle(Argument("STRUCTURE", "What kind of structure is being built?", ", ".join(structures.keys())))]
+Structure = structures[argument_handler.handle(Argument("STRUCTURE", "What kind of structure is being built?", ", ".join(structures.keys())))]
 
 try:
-	commands = generate(structure(), argument_handler)
+	commands = generate(Structure(), argument_handler)
 	command_output = AutoTyper()
 	for count, command in enumerate(commands):
 		print("Entering command {} of {}:{}".format(count+1, len(commands), command))
