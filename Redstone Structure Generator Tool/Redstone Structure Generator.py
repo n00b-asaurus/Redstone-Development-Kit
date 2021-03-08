37993ca943f99027d3fe04b49717bf43ceb8ddcb
Bob Vicktor <TheRealBobVicktor@gmail.com>
2021-03-08 12:21:19 -0300

    Added arguments for selecting which structure to build.

--- Redstone Structure Generator Tool/Redstone Structure Generator.py
+++ Redstone Structure Generator Tool/Redstone Structure Generator.py
@@ -5,6 +5,7 @@ from structures.basic_encoder.encoder import Encoder as BasicEncoder
 from structures.properinglish19_decoder.decoder import Decoder as ProperinglishDecoder
 from structures.stenodyon_decoder.decoder import Decoder as StenodyonDecoder
 from string_handlers.autotyper import AutoTyper
+from argparse import ArgumentParser
 import traceback
 
 structures = {
@@ -15,8 +16,16 @@ structures = {
 
 argument_handler = ArgumentHandler()
 
-def main(): 
-	Structure = structures[argument_handler.handle(Argument("STRUCTURE", "What kind of structure is being built?", ", ".join(structures.keys())))]
+def main(structure=None):
+	if structure in structures.keys():
+		Structure = structures[structure]
+	elif structure in structures.values():
+		Structure = structure
+	else:
+		if structure:
+			# If we got here and structure is set then the user gave bad input
+			print(f"'{structure}' isn't a valid structure name.")
+		Structure = structures[argument_handler.handle(Argument("STRUCTURE", "What kind of structure is being built?", ", ".join(structures.keys())))]
 
 	try:
 		commands = generate(Structure(), argument_handler)
@@ -31,4 +40,12 @@ def main():
 	input("Press Enter to Continue...")
 
 if __name__ == "__main__":
-	main()
+	parser = ArgumentParser()
+	structure_arg_group = parser.add_mutually_exclusive_group()
+	structure_arg_group.add_argument("-s", "--structure", help="The name of the structure to build (must match exactly).")
+	structure_arg_group.add_argument("-e", "--basic_encoder", action="store_const", const="basic_encoder", dest="structure", help="Build the basic_encoder.")
+	structure_arg_group.add_argument("-d", "--stenodyon_decoder", action="store_const", const="stenodyon_decoder", dest="structure", help="Build the stenodyon_decoder.")
+	structure_arg_group.add_argument("-p", "--properinglish19_decoder", action="store_const", const="properinglish19_decoder", dest="structure", help="Build the properinglish19_decoder.")
+
+	args = parser.parse_args()
+	main(args.structure)
