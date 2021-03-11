6c85f0308c5da06a2eb6f440d9af3f1fe81eb895
n00b-asaurus <36574472+n00b-asaurus@users.noreply.github.com>
2021-03-10 18:56:45 -0600

    Added output argument to the structure generator
    This will allow the user to select whether they type the structure out into the chat or save it to an mcfunction

--- Redstone Structure Generator Tool/Redstone Structure Generator.py
+++ Redstone Structure Generator Tool/Redstone Structure Generator.py
@@ -4,7 +4,7 @@ from structure_generator.argument import Argument
 from structures.basic_encoder.encoder import Encoder as BasicEncoder
 from structures.properinglish19_decoder.decoder import Decoder as ProperinglishDecoder
 from structures.stenodyon_decoder.decoder import Decoder as StenodyonDecoder
-#from string_handlers.autotyper import AutoTyper
+from string_handlers.autotyper import AutoTyper
 from string_handlers.mcfunction_generator import McFunctionGenerator
 from argparse import ArgumentParser
 import traceback
@@ -15,9 +15,21 @@ structures = {
 	"properinglish19_decoder": ProperinglishDecoder,
 }
 
+outputs = {
+	"autotyper": AutoTyper,
+	"mcfunction": McFunctionGenerator,
+}
+
 argument_handler = ArgumentHandler()
 
-def main(structure=None):
+def pick_class_from_dictionary(name, help, dictionary):
+	valid_options = ", ".join(dictionary.keys())
+	users_choice = Argument(name, help, valid_options)
+	key = argument_handler.handle(users_choice)
+	return dictionary[key]
+	
+
+def main(structure=None, output=None):
 	if structure in structures.keys():
 		Structure = structures[structure]
 	elif structure in structures.values():
@@ -26,11 +38,14 @@ def main(structure=None):
 		if structure:
 			# If we got here and structure is set then the user gave bad input
 			print(f"'{structure}' isn't a valid structure name.")
-		Structure = structures[argument_handler.handle(Argument("STRUCTURE", "What kind of structure is being built?", ", ".join(structures.keys())))]
+		Structure = pick_class_from_dictionary("STRUCTURE", "what kind of structure is being built?", structures)
 
 	try:
 		commands = generate(Structure(), argument_handler)
-		command_output = McFunctionGenerator()
+		if output == None:
+			command_output = pick_class_from_dictionary("OUTPUT", "how would you like to process this structure", outputs)()
+		else:
+			command_output = outputs[output]()
 		command_output.handle(commands)
 		command_output.close()
 	except Exception as e:
@@ -45,6 +60,9 @@ if __name__ == "__main__":
 	structure_arg_group.add_argument("-e", "--basic_encoder", action="store_const", const="basic_encoder", dest="structure", help="Build the basic_encoder.")
 	structure_arg_group.add_argument("-d", "--stenodyon_decoder", action="store_const", const="stenodyon_decoder", dest="structure", help="Build the stenodyon_decoder.")
 	structure_arg_group.add_argument("-p", "--properinglish19_decoder", action="store_const", const="properinglish19_decoder", dest="structure", help="Build the properinglish19_decoder.")
+	output_group = parser.add_mutually_exclusive_group()
+	output_group.add_argument("-a","--autotyper",action="store_const", const="autotyper", dest="output", help="Type the build commands directly into chat")
+	output_group.add_argument("-m","--mcfunction",action="store_const", const="mcfunction", dest="output", help="Save the build commands to an mcfunction file")
 
 	args = parser.parse_args()
-	main(args.structure)
+	main(args.structure, args.output)
